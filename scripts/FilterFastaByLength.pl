225036a96783ab00bd9ea8adabff6785354da2c5
yaxalponce <37907862+yaxalponce@users.noreply.github.com>
2022-02-27 23:07:23 +0100

    Perl script to filter sequences by length

---
+++ scripts/FilterFastaByLength.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# Usage: perl FilterFastaByLength.pl #MinLength file.fasta >FilteredFile.fasta
+
+use strict;
+use warnings;
+
+my $minlen = shift or die "Error: `minlen` parameter not provided\n";
+{
+    local $/=">";
+    while(<>) {
+        chomp;
+        next unless /\w/;
+        s/>$//gs;
+        my @chunk = split /\n/;
+        my $header = shift @chunk;
+        my $seqlen = length join "", @chunk;
+        print ">$_" if($seqlen >= $minlen);
+    }
+    local $/="\n";
+}
